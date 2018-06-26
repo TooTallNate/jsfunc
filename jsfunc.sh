21dce3a51bebfdbb43948945e3c0a6bb7ead6af1
Nathan Rajlich <nathan@tootallnate.net>
2018-06-25 22:38:48 -0700

    Prettyify

--- jsfunc.sh
+++ jsfunc.sh
@@ -4,10 +4,24 @@ jsfunc() {
   local fn="$(cat <<EOFF
 $1() {
   node <(cat <<EOF
-require('stream').Readable.prototype.then = function (...args) { return new Promise((res, rej) => { const bufs = []; this.on('error', rej).on('data', buf => bufs.push(buf)).on('end', () => res(Buffer.concat(bufs))); }).then(...args) };
+require('stream').Readable.prototype.then = function (...args) {
+  return new Promise((res, rej) => {
+    const bufs = [];
+    this.on('error', rej).on('data', buf => bufs.push(buf))
+        .on('end', () => res(Buffer.concat(bufs))); }).then(...args);
+};
 (async () => {
 ${code}
-})().then(val => typeof val !== 'undefined' && console.log(typeof val === 'string' ? val : JSON.stringify(val, null, 2))).catch(err => console.error(err.stack) || process.exit(1));
+})()
+  .then(val => {
+    const type = typeof val;
+    if (type !== 'undefined') {
+      console.log(type === 'string' ? val : JSON.stringify(val, null, 2));
+    }
+  }).catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+  });
 EOF
 ) "\$@"
 }
